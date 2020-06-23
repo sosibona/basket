d3ddb0daf91419fa3efae05da6ec8deaa3791348
sosibona <empty.tara@gmail.com>
2020-06-23 03:57:21 +0300

    delete comments

--- src/components/BasketList/BasketList.js
+++ src/components/BasketList/BasketList.js
@@ -7,56 +7,6 @@ import { sortedSelector } from "../../products/products.selectors";
 import { sortProduct, getProductList } from "../../products/products.actions";
 import PropTypes from "prop-types";
 
-// class BasketList extends React.Component {
-//   componentDidMount() {
-//     this.props.getProductList();
-//   }
-//   render() {
-//     const { sortProducts, products } = this.props;
-//     const productsList = products.map((item) => {
-//       return <Product key={item.id} {...item} />;
-//     });
-//     return (
-//       <div>
-//         <table className="table">
-//           <thead>
-//             <tr>
-//               <th
-//                 className="table-header__name"
-//                 onClick={() => sortProducts("name")}
-//               >
-//                 Name of product
-//               </th>
-//               <th
-//                 className="table-header__count"
-//                 onClick={() => sortProducts("count")}
-//               >
-//                 Count &uarr;&darr;
-//               </th>
-//               <th
-//                 className="table-header__price"
-//                 onClick={() => sortProducts("pricePerOne")}
-//               >
-//                 Price per one item &uarr;&darr;
-//               </th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {productsList.length ? (
-//               productsList
-//             ) : (
-//               <tr>
-//                 <td colSpan="3">No products</td>
-//               </tr>
-//             )}
-//             <Total />
-//           </tbody>
-//         </table>
-//       </div>
-//     );
-//   }
-// }
-
 const BasketList = ({ products, sortProducts, getProductList }) => {
   useEffect(() => {
     getProductList();
